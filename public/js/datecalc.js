8fea79fdcce5f4350f0b8e1d64460814311f1e62
Carlos Lezama <carloselezamaj@gmail.com>
2021-05-31 12:01:52 -0500

    Update error.html

--- public/js/datecalc.js
+++ public/js/datecalc.js
@@ -1,4 +1,4 @@
-var then = new Date("2019-06-20T00:00:00.000-05:00");
+var then = new Date("2019-06-20T16:00:00.000-05:00");
 now = new Date;
 diff = Math.round((now - then) / (1000 * 60 * 60 * 24)) - 1;
 document.write(diff);
